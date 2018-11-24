7a5e35257b54eac67d0c47f6c7efb5f7e8b2cdd2
TIAGO-PC\Tiago Silvestre <>
2018-11-24 15:16:07 +0000

    Mana Spawner basic mechanisms and options

--- Assets/Scripts/Arena/ManaSpawnerScript.cs
+++ Assets/Scripts/Arena/ManaSpawnerScript.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class ManaSpawnerScript : MonoBehaviour {
 
     public GameObject manaPot;
+    int potCounter = 0;
     float randomX;
     float randomY;
     Vector2 spawnerLocation;
     public float spawnRate = 2f;
-    float nextSpawn = 5.0f;
+    public int spawnLimit = 5;
+    public float spawnTime = 0.0f;
     	
 	// Update is called once per frame
 	void Update () {
 		
-        if(Time.time > nextSpawn)
-        {
-            nextSpawn = Time.time + spawnRate;
-            randomX = Random.Range(-10.0f, 10.0f);
-            randomY = Random.Range(-4.0f, 4.0f);
-            spawnerLocation = new Vector2(randomX, randomY);
-            Instantiate(manaPot, spawnerLocation, Quaternion.identity);
+        if(potCounter < spawnLimit) { 
+            if(Time.time > spawnTime)
+            {
+                spawnTime = Time.time + spawnRate;
+                randomX = Random.Range(-10.0f, 10.0f);
+                randomY = Random.Range(-4.0f, 4.0f);
+                spawnerLocation = new Vector2(randomX, randomY);
+                Instantiate(manaPot, spawnerLocation, Quaternion.identity);
+                potCounter++;
+            }
         }
-	}
+    }
 }
