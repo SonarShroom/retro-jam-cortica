ee9dda3dcacfbae4ddac675c527ffa06d1ffe8ba
sonersound <mario.shadic.esteves@hotmail.com>
2018-11-24 12:33:39 +0000

    Added misc files, player resources now holds player health (curr and max), and player shots (current and max)

---
+++ Assets/Scripts/Player/PlayerResources.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerResources : MonoBehaviour
+{
+	[SerializeAttribute]
+	private uint m_playerHealth, m_maxPlayerHealth, m_playerShots, m_maxPlayerShots;
+
+	public uint PlayerHealth
+	{
+		get { return m_playerHealth; }
+		set { m_playerHealth = value; }
+	}
+
+	public uint PlayerShots
+	{
+		get { return m_playerShots; }
+		set { m_playerShots = value; }
+	}
+	public void OnPlayerShoot()
+	{
+		m_playerShots--;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		//TODO: if has component of type collectible
+		//and player shots still less than max shots
+		//add one to player shots and remove object from play
+
+		//TODO: if has component of type spell,
+		//check who owner and if different, take damage.
+	}
+
+}
