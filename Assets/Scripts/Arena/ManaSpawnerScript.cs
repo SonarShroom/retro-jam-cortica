2a514404dcb7a29aca6c2c928ee25c3091e467cb
TIAGO-PC\Tiago Silvestre <>
2018-11-24 18:48:13 +0000

    More ManaSpawner tweaks adding first spawner timer

--- Assets/Scripts/Arena/ManaSpawnerScript.cs
+++ Assets/Scripts/Arena/ManaSpawnerScript.cs
@@ -9,9 +9,11 @@ public class ManaSpawnerScript : MonoBehaviour {
     float randomX;
     float randomY;
     Vector2 spawnerLocation;
-    public float spawnRate = 2f;
-    public int spawnLimit = 5;
+    public float spawnRate = 2.0f;
     public float spawnTime = 0.0f;
+    public float firstSpawnTime = 2.0f;
+    bool hasFirstSpawn = false;
+    public int spawnLimit = 5;
 
     private void Start()
     {
@@ -19,25 +21,44 @@ public class ManaSpawnerScript : MonoBehaviour {
     }
     
     void Update () {
-        spawnTime += Time.deltaTime;
-        if (pots.Count < spawnLimit) {
-            if (spawnTime > spawnRate) {
-                spawnTime += Time.deltaTime;
-                randomX = Random.Range(-10.0f, 10.0f);
-                randomY = Random.Range(-4.0f, 4.0f);
-                spawnerLocation = new Vector2(randomX, randomY);
-                pots.Add(Instantiate(manaPot, spawnerLocation, Quaternion.identity));
+        if (hasFirstSpawn == true)
+        {
+            spawnTime += Time.deltaTime;
+            if (pots.Count < spawnLimit)
+            {
+                if (spawnTime > spawnRate)
+                {
+                    InstantiateManaPotLocation();
+                }
+            }
+            else
+            {
                 spawnTime = 0.0f;
             }
-        }
-        else
+        } else
         {
-            spawnTime = 0.0f;
+            spawnTime += Time.deltaTime;
+            if(spawnTime > firstSpawnTime)
+            {
+                InstantiateManaPotLocation();
+                hasFirstSpawn = true;
+            }
+
         }
     }
 
     public void RemoveManaPot(GameObject manaPotToRemove)
     {
         pots.Remove(manaPotToRemove);
+    }
+
+    public void InstantiateManaPotLocation()
+    {
+        spawnTime += Time.deltaTime;
+        randomX = Random.Range(-10.0f, 10.0f);
+        randomY = Random.Range(-4.0f, 4.0f);
+        spawnerLocation = new Vector2(randomX, randomY);
+        pots.Add(Instantiate(manaPot, spawnerLocation, Quaternion.identity));
+        spawnTime = 0.0f;
     }
 }
