28862bad185dd6f148d282db04b656720fed19f3
sonersound <mario.shadic.esteves@hotmail.com>
2018-11-25 16:31:50 +0000

    last push before doom

---
+++ Assets/Scripts/GameMode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamepadInput;
+
+public class GameMode : MonoBehaviour {
+
+	public List<GameObject> objectsActiveOnStart;
+	
+	// Update is called once per frame
+	void Update () {
+		if(GamePad.GetButtonDown(GamePad.Button.A, GamePad.Index.Any))
+		{
+			foreach(GameObject go in objectsActiveOnStart)
+			{
+				go.SetActive(true);
+			}
+			gameObject.SetActive(false);
+		}
+	}
+}
