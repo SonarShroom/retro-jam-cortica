29d12a80cef901e949baad40409884b9d4e5a36e
sonersound <mario.shadic.esteves@hotmail.com>
2018-11-25 16:59:01 +0000

    oh boi here we go

--- Assets/Scripts/UI/AmmoTracker.cs
+++ Assets/Scripts/UI/AmmoTracker.cs
@@ -11,6 +11,8 @@ public class AmmoTracker : MonoBehaviour {
 	private List<Sprite> m_ammoIndicators;
 	
 	void Update () {
+		if(!m_playerToTrack)
+			return;
 		int _currentPlayerAmmo = (int)m_playerToTrack.GetComponent<PlayerResources>().m_playerShots;
 		GetComponent<Image>().sprite = m_ammoIndicators[_currentPlayerAmmo - 1];
 	}
