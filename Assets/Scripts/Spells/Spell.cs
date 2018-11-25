460a39d2c4731a2deb6a0daf99644666f34453b5
TIAGO-PC\Tiago Silvestre <>
2018-11-25 08:14:04 +0000

    Spells destroy each other

--- Assets/Scripts/Spells/Spell.cs
+++ Assets/Scripts/Spells/Spell.cs
@@ -11,4 +11,13 @@ public class Spell : MonoBehaviour {
 		Rigidbody2D _rigidbody = GetComponent<Rigidbody2D>();
 		_rigidbody.velocity = inputVector * m_spellSpeed;
 	}
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Contains("Spell"))
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+    }
 }
